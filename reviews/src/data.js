0ce04763493b78cddc43fc6808cf8a1b707965cc
Miguel Alves <miguelpalves01@gmail.com>
2024-11-26 10:20:19 -0300

    reviews added

---
+++ reviews/src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    text: 'Working here has been a transformative experience. The team fosters creativity and collaboration, making every project enjoyable and rewarding. The company truly values innovation and growth.',
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    text: 'This is the perfect place for anyone passionate about design. The leadership is supportive, and the work environment is inspiring. I’ve grown tremendously in my role thanks to the opportunities provided here.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    text: 'As an intern, I’ve been given real responsibilities and the chance to learn from some of the best professionals in the industry. The mentorship and experience here are unparalleled.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    text: 'Leading this company has been an incredible journey. I’m proud of the culture we’ve built, where every employee feels empowered to contribute and make a difference. Together, we’re achieving amazing things.',
+  },
+]
+
+export default reviews
