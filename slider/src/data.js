5913f6a6b108b96f6cea01c045576ea794e41c53
Miguel Alves <miguelpalves01@gmail.com>
2025-01-07 15:56:43 -0300

    'slider-added'

---
+++ slider/src/data.js
@@ -0,0 +1,112 @@
+export const shortList = [
+  {
+    id: 1,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'Maria Ferguson',
+    title: 'Project Manager',
+    quote:
+      'I thrive in fast-paced environments, turning challenges into opportunities and leading teams to success.',
+  },
+];
+
+export const list = [
+  {
+    id: 1,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'Maria Ferguson',
+    title: 'Project Manager',
+    quote:
+      'I thrive in fast-paced environments, turning challenges into opportunities and leading teams to success.',
+  },
+  {
+    id: 2,
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    name: 'John Doe',
+    title: 'Software Developer',
+    quote:
+      'Innovation and creativity drive my approach to solving complex technical problems.',
+  },
+  {
+    id: 3,
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'Peter Smith',
+    title: 'UX Designer',
+    quote:
+      'Designing intuitive user experiences that prioritize functionality and aesthetics is my passion.',
+  },
+  {
+    id: 4,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'Susan Andersen',
+    title: 'CEO',
+    quote:
+      'Leadership is about empowering others, fostering innovation, and driving organizational growth.',
+  },
+];
+
+export const longList = [
+  {
+    id: 1,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'Maria Ferguson',
+    title: 'Project Manager',
+    quote:
+      'I thrive in fast-paced environments, turning challenges into opportunities and leading teams to success.',
+  },
+  {
+    id: 2,
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    name: 'John Doe',
+    title: 'Software Developer',
+    quote:
+      'Innovation and creativity drive my approach to solving complex technical problems.',
+  },
+  {
+    id: 3,
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'Peter Smith',
+    title: 'UX Designer',
+    quote:
+      'Designing intuitive user experiences that prioritize functionality and aesthetics is my passion.',
+  },
+  {
+    id: 4,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'Susan Andersen',
+    title: 'CEO',
+    quote:
+      'Leadership is about empowering others, fostering innovation, and driving organizational growth.',
+  },
+  {
+    id: 5,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'Maria Ferguson',
+    title: 'Project Manager',
+    quote:
+      'I thrive in fast-paced environments, turning challenges into opportunities and leading teams to success.',
+  },
+  {
+    id: 6,
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    name: 'John Doe',
+    title: 'Software Developer',
+    quote:
+      'Innovation and creativity drive my approach to solving complex technical problems.',
+  },
+  {
+    id: 7,
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'Peter Smith',
+    title: 'UX Designer',
+    quote:
+      'Designing intuitive user experiences that prioritize functionality and aesthetics is my passion.',
+  },
+  {
+    id: 8,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'Susan Andersen',
+    title: 'CEO',
+    quote:
+      'Leadership is about empowering others, fostering innovation, and driving organizational growth.',
+  },
+];
